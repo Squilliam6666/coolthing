cc61b439634740c02a150466648b191fffc2b451
Squilliam6666 <wcmerr5398@ung.edu>
2026-02-27 15:21:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-document.body.innerHTML = "<h1>Hello from JavaScript!</h1>";
+document.body.innerHTML = "<h1>Helo from JavaScript!</h1>";
 
 //https://www.imdb.com/search/title/?title_type=feature&num_votes=100000,&sort=alpha,asc
 console.log(10);
+
